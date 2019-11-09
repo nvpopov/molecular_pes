c4bb4f669fb848de957614f54d2c689ffc4e259a
nvpopov <brodiaga38@gmail.com>
2019-11-09 23:09:26 +0300

    added pes_extarctor_ng

---
+++ sbin/pes_extract_ng.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from math import *
+import os
+import sys
+from operator import itemgetter
+
+batchdir = "batch"
+
+task_list = os.listdir(batchdir)
+
+output_batch = []
+
+for task_id, task in enumerate(task_list):
+    #print ("{} {}".format(task_id, task))
+
+    batch_dir = "batch/{}".format(task)
+    #opend DATA file
+    data_file = open("{}/DATA".format(batch_dir), "r")
+
+    total_batch_in_file = 0
+    batch_data = []
+    for data_file_line in data_file.readlines():
+        total_batch_in_file += 1
+        data_file_ls = data_file_line.split()
+        batch_data_line = []
+        for df_ls in data_file_ls:
+            batch_data_line.append(float(df_ls))
+        batch_data.append(batch_data_line)
+    
+    #print(batch_data)
+    
+    outfile = open("{}/orca.out".format(batch_dir), "r")
+    
+    reads_hit = 0
+    batch_id = 0
+
+    energies_bsse = []
+
+    for out_line in outfile.readlines():
+        
+        if "Total Energy       :" in out_line:    
+            out_line_split = out_line.split()
+            en = float(out_line_split[3])
+            reads_hit+=1
+            energies_bsse.append(en)
+
+            if reads_hit > 2:
+                data_for_current_batch = [elem for elem in batch_data[batch_id]]
+
+                for i in range(0,3):
+                    data_for_current_batch.append(energies_bsse[i])
+
+                output_batch.append(data_for_current_batch)
+                reads_hit = 0
+                batch_id+=1
+                energies_bsse = []
+
+sorted_output_batch = sorted(output_batch, key=itemgetter(0,1))
+
+
+for srt_b in sorted_output_batch:
+    bsse_corrected_en = srt_b[2] - srt_b[3] - srt_b[4]
+    print("{},{},{},{},{},{}".format(srt_b[0], srt_b[1], bsse_corrected_en, srt_b[2], srt_b[3], srt_b[4]))

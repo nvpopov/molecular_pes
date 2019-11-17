316a07324b9862d17000beb034cc5d56de196838
nvpopov <brodiaga38@gmail.com>
2019-11-17 12:47:17 +0300

    feat: introduced task list

--- sbin/generator_ng.py
+++ sbin/generator_ng.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-from math import pi,sin,cos,sqrt,pow
+from math import pi,sin,cos,sqrt,pow, degrees
 from enum import Enum
 import sys, os
 import itertools
+
+bohr2angs = 0.529177
 
 d_N2 = 1.099998  
 d_N2_SolidState = 1.06  
@@ -14,8 +16,8 @@ DEFAULT_ORCA_HEADER = """! RHF  DLPNO-CCSD(T) aug-cc-pVQZ aug-cc-pVQZ/C RIJCOSX 
 end"""
 
 CCSDT_CBS_ORCA_HEADER = """! RHF CCSD(T) Extrapolate(4/5,aug-cc) VERYTIGHTSCF  DIRECT PMODEL
-%maxcore 3000
-%pal nprocs 3 
+%maxcore 5000
+%pal nprocs 9 
 end"""
 
 AUG_CCPVXZ_BASIS_NAMES = {
@@ -31,6 +33,60 @@ CCPVXZ_BASIS_NAMES = {
     4 : "cc-pVQZ",
     5 : "cc-pV5Z"
 }
+
+N2_Ar_DISTANCES_BOHR = [
+    4.75, 5.00, 5.25, 5.50, 5.75, 6.00, 
+    6.25, 6.50, 6.75, 7.00, 7.25, 7.50, 
+    7.75, 8.00, 8.25, 8.50, 8.75, 9.00,
+    9.25, 9.50, 9.75, 10.0, 11.0, 12.0,
+    14.0, 16.0, 18.0, 20.0, 25.0, 30.0
+]
+
+N2_Ar_DISTANCES = [r*bohr2angs for r in N2_Ar_DISTANCES_BOHR ]
+
+LEGENDRE_ROOTS_ANGLE_ARCCOS = [
+    2.963498195463516,
+    2.732792416247772,
+    2.500726327116406,
+    2.268356043649630,
+    2.035874587341303,
+    1.803344977489339,
+    1.570796326794897,
+    1.338247676100454,
+    1.105718066248490,
+    0.8732366099401631,
+    0.6408663264733868,
+    0.4088002373420213,
+    0.1780944581262768
+]
+
+LEGENDRE_ROOTS_ANGLE_WITH_ZERO = [
+    2.963498195463516,
+    2.732792416247772,
+    2.500726327116406,
+    2.268356043649630,
+    2.035874587341303,
+    1.803344977489339,
+    1.570796326794897,
+    1.338247676100454,
+    1.105718066248490,
+    0.8732366099401631,
+    0.6408663264733868,
+    0.4088002373420213,
+    0.1780944581262768,
+    0.0000000000000000
+]
+
+LEGENDRE_ROOTS_ANGLE_WITH_0_HALFPI = [
+    1.570796326794897,
+    1.338247676100454,
+    1.105718066248490,
+    0.8732366099401631,
+    0.6408663264733868,
+    0.4088002373420213,
+    0.1780944581262768,
+    0.0000000000000000
+]
 
 class ModelType(Enum):
     UNKNOWN = 1
@@ -169,6 +225,116 @@ def cb_linear_mol_vs_single_atom(linear_molecule, single_atom, outdir, batch_per
 
     pass
 
+def cb_linear_mol_vs_single_atom_mesh(linear_molecule, single_atom, outdir, batch_per_input,
+                                      r_mesh, angle_mesh, doCBS ):
+
+    #compose tasks root directory
+
+    if os.path.exists(".//batch"):
+        print("Batch directory is exsists. Abort.")
+        exit()
+
+    task_root_dir = "{}/batch".format(outdir)
+    os.mkdir(task_root_dir)
+
+    range_R = range(0, len(r_mesh))
+    range_phi = range(0, len(angle_mesh))
+    print("Total num. of steps by r = {}".format(len(r_mesh)))
+    print("Total num. of steps by angle = {}".format(len(angle_mesh)))
+
+    batch_id = 0
+    batch_file = None
+    batch_file_domain = None
+
+    xyz_file = open("anim.xyz", "w")
+
+    for q, i in enumerate(itertools.product(range_R, range_phi)):
+
+        if (q % batch_per_input == 0):
+            batch_dir = "{}/batch/t_{}".format(outdir, batch_id)
+            os.mkdir(batch_dir)
+
+            batch_file_path = "{}/orca.inp".format(batch_dir)
+            batch_file_domain_path = "{}/DATA".format(batch_dir)
+            batch_file_domain = open(batch_file_domain_path, "w")
+            batch_file = open(batch_file_path, "w")
+
+            batch_id += 1
+
+        ir = i[0]
+        iphi = i[1]
+
+        xyz_file.write("{}\n\n".format(len(linear_molecule.atoms)+1))
+        #compose task directory
+       
+        #N1 = [d_N2/2, 0, 0]
+        #N2 = [-d_N2/2, 0, 0]
+
+    
+        #Emit dimer AB
+        if (batch_per_input > 1):
+            pass
+        
+        if doCBS:
+            batch_file.write("{}\n".format(CCSDT_CBS_ORCA_HEADER))
+        else:
+            batch_file.write("{}\n".format(DEFAULT_ORCA_HEADER))
+
+        batch_file.write("* xyz 0 1\n")
+        for atom in linear_molecule.atoms:
+            batch_file.write("{} {} {} {}\n".format(atom.name, atom.coord[0], atom.coord[1], atom.coord[2]))
+            xyz_file.write("{} {} {} {}\n".format(atom.name, atom.coord[0], atom.coord[1], atom.coord[2]))
+        
+        SA_R = r_mesh[ir]
+        SA_phi = angle_mesh[iphi]
+        SA = [SA_R * cos(SA_phi), SA_R * sin(SA_phi),0]
+        
+        dists_to_lm = []
+        for atom_lm in linear_molecule.atoms:
+            dist = pow(atom_lm.coord[0] - SA_R * cos(SA_phi), 2) + \
+            pow(atom_lm.coord[1] - SA_R * sin(SA_phi), 2) + \
+            pow(atom_lm.coord[2] - 0, 2) 
+            dist_sqrt = sqrt(dist)
+            dists_to_lm.append(dist_sqrt)
+
+        dists_to_lm_str = " ".join(map(str, dists_to_lm))
+
+        batch_file_domain.write("{} {} {}\n".format(SA_R, SA_phi, dists_to_lm_str))
+
+        SA = [SA_R * cos(SA_phi), SA_R * sin(SA_phi),0]
+        batch_file.write("{} {} {} {}\n".format(single_atom.atoms[0].name, SA[0], SA[1], SA[2]))
+        xyz_file.write("{} {} {} {}\n".format(single_atom.atoms[0].name, SA[0], SA[1], SA[2]))
+        batch_file.write("end\n\n")
+
+        #Emit monomer A with ghost atoms(B)
+        batch_file.write("$new_job\n")
+        if doCBS:
+            batch_file.write("{}\n".format(CCSDT_CBS_ORCA_HEADER))
+        else:
+            batch_file.write("{}\n".format(DEFAULT_ORCA_HEADER))
+
+        batch_file.write("* xyz 0 1\n")
+        for atom in linear_molecule.atoms:
+            batch_file.write("{} {} {} {}\n".format(atom.name, atom.coord[0], atom.coord[1], atom.coord[2]))
+        
+        batch_file.write("{}: {} {} {}\n".format(single_atom.atoms[0].name, SA[0], SA[1], SA[2]))
+        batch_file.write("end\n\n")
+
+        #Emit monomer B with ghost atoms(A)
+        batch_file.write("$new_job\n")
+        if doCBS:
+            batch_file.write("{}\n".format(CCSDT_CBS_ORCA_HEADER))
+        else:
+            batch_file.write("{}\n".format(DEFAULT_ORCA_HEADER))
+
+        batch_file.write("* xyz 0 1\n")
+        for atom in linear_molecule.atoms:
+            batch_file.write("{}: {} {} {}\n".format(atom.name, atom.coord[0], atom.coord[1], atom.coord[2]))
+        batch_file.write("{} {} {} {}\n".format(single_atom.atoms[0].name, SA[0], SA[1], SA[2]))
+        batch_file.write("end\n\n")
+
+    pass
+
 def sample_tasks_N2_Ar_pes(N2dist):
     N2_mol = Molecule()
     N2_mol.add_atom("N", [N2dist/2, 0, 0])
@@ -189,7 +355,7 @@ def sample_tasks_N2_Ne_pes(N2dist):
 
     cb_linear_mol_vs_single_atom(N2_mol, Ne_mol, "./", 1)
 
-def sample_tasks_N2_Ar_pes_CBS(N2dist):
+def sample_tasks_N2_Ar_pes_cbs(N2dist):
     N2_mol = Molecule()
     N2_mol.add_atom("N", [N2dist/2, 0, 0])
     N2_mol.add_atom("N", [-N2dist/2, 0, 0])
@@ -199,8 +365,64 @@ def sample_tasks_N2_Ar_pes_CBS(N2dist):
 
     cb_linear_mol_vs_single_atom(N2_mol, Ar_mol, "./", 1, 1.7, 10,  50,  0e0, pi/2e0,  15,  True)
 
+def sample_tasks_N2_X_pes_cbs_mesh(N2dist, single_atom_name):
+    N2_mol = Molecule()
+    N2_mol.add_atom("N", [N2dist/2, 0, 0])
+    N2_mol.add_atom("N", [-N2dist/2, 0, 0])
+
+    X_mol = Molecule()
+    X_mol.add_atom(single_atom_name, [0, 0, 0])
+    #print(type(N2dist))
+    #print(type(single_atom_name))
+    cb_linear_mol_vs_single_atom_mesh(N2_mol, X_mol, "./", 1, N2_Ar_DISTANCES, LEGENDRE_ROOTS_ANGLE_WITH_0_HALFPI, True)
+
+def debug_plot_normalized_angle_mesh(*args):
+    for elem in LEGENDRE_ROOTS_ANGLE_ARCCOS:
+        print("{0:.12f} {1:.12f}".format(elem, degrees(elem)))
+    pass
+
+def debug_plot_normalized_angle_mesh_zero_hfpi(*args):
+    for elem in LEGENDRE_ROOTS_ANGLE_WITH_0_HALFPI:
+        print("{0:.12f} {1:.12f}".format(elem, degrees(elem)))
+    pass
+
+def print_all_tasks(task_list):
+    print("Avaiable tasks:")
+    for task_idx, task in enumerate(task_list):
+        print("{}){}".format(task_idx, task[0]))
+
 def app_main():
-    sample_tasks_N2_Ar_pes_CBS(d_N2)
+
+    pos_args = tuple([sys.argv[i] for i in range(2, len(sys.argv))])
+    print("pos_args = {}".format(pos_args))
+
+    task_list = []
+    task_list.append(["list", print_all_tasks, (task_list,), 0])
+    task_list.append(["n2_ar_pes_cbs", sample_tasks_N2_Ar_pes_cbs, (d_N2,), 0])
+    task_list.append(["n2_x_pes_cbs_mesh", sample_tasks_N2_X_pes_cbs_mesh, (d_N2,), 1])
+    task_list.append(["gm", debug_plot_normalized_angle_mesh, (), 0])
+    task_list.append(["gm0hpi", debug_plot_normalized_angle_mesh_zero_hfpi, (), 0])
+
+    task = task_list[0][0]
+
+    if (len(sys.argv) != 1):
+        task = sys.argv[1]
+    print("Executing task = {}".format(task))
+
+    task_founded = False
+
+    for task_it in task_list:
+        if task == task_it[0]:
+            if task_it[3] == 0:
+                task_it[1](task_it[2])
+            else:
+                task_it[1](*task_it[2], *pos_args)
+            task_founded = True
+            pass
+    
+    if (task_founded == False):
+        print("Invalid task")
+
     pass
 
 if __name__ == "__main__":
